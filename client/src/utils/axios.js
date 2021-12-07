27c1d0eb230b0a6ca428e067d7dbefd36f64add0
shahjalal <lx.shahjalall@gmail.com>
2021-12-08 02:08:50 +0600

    changing api uri

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	// baseURL: 'https://ecom22.herokuapp.com/',
-	baseURL: 'http://localhost:5000/',
+	baseURL: 'https://ecom22.herokuapp.com/',
+	// baseURL: 'http://localhost:5000/',
 });
 
 instance.interceptors.request.use(
