43c152212a906c01700e373a2619180fa8ebef35
shahjalal <lx.shahjalal@gmail.com>
2025-04-29 18:06:39 +0600

    chore: remove redux-devtools-extension dependency
    The redux-devtools-extension package has been deprecated and moved to @redux-devtools/extension. This commit removes the deprecated package and updates the store configuration to no longer use it.

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,7 +1,6 @@
-import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import reducer from './rootReducer';
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import reducer from "./rootReducer";
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(reducer, applyMiddleware(thunk));
 export default store;
